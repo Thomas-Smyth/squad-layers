c91a4210d6c1403f224b9a8162718da5cb470da6
Thomas Smyth <thomas.smyth3141@gmail.com>
2020-02-21 22:42:02 +0000

    Fix Jensen's layer names

--- updater/classname-converter.js
+++ updater/classname-converter.js
@@ -3,7 +3,8 @@ export default function (name, layer = true) {
         .replace(/ /g, '_')
         .replace('\'', '')
         .replace('Kokan', 'Kokan_Valley')
-        .replace('Manic-5', 'Manic');
+        .replace('Manic-5', 'Manic')
+        .replace('Training_v', 'v');
 
     if(layer){
         name = name
